a06e071cf6d5e0761eea7dad1931fa203df0b099
Dreamwings <huyb2009@gmail.com>
2024-03-24 17:21:56 -0700

    Time: 26 ms (98.42%) | Memory: 16.5 MB (61.30%) - LeetSync

---
+++ 65-valid-number/valid-number.py
@@ -0,0 +1,47 @@
+class Solution:
+    def isNumber(self, s: str) -> bool:
+        """
+        ## S1:
+        # special = {"inf", "-inf", "+inf", "Infinity", "infinity", "+Infinity", "-Infinity", "+infinity", "-infinity", "nan"}
+
+        try:
+            if "inf" in s or "Inf" in s or "nan" in s: # s in special
+                return False
+            num = float(s)
+            return True
+        except:
+            return False
+
+        """
+        ## S2:
+
+        s = s.strip()
+        if not s:
+            return False
+
+        seenNum = False
+        seenDot = False
+        seenE = False
+
+        for i, c in enumerate(s):
+            if c == '.':
+                if seenDot or seenE:
+                    return False
+                seenDot = True
+            elif c == 'e' or c == 'E':
+                if seenE or not seenNum:
+                    return False
+                seenE = True
+                seenNum = False
+            elif c in '+-':
+                if i > 0 and s[i - 1] not in 'eE':
+                    return False
+                seenNum = False
+            else:
+                if not c.isdigit():
+                    return False
+                seenNum = True
+
+        return seenNum
+        
+        
