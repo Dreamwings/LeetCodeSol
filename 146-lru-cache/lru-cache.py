8df5ed6a667de46fe977d6a4e9c18b74fcac03d9
Dreamwings <huyb2009@gmail.com>
2024-03-25 10:30:36 -0700

    Time: 625 ms (14.80%) | Memory: 76.7 MB (99.62%) - LeetSync

---
+++ 146-lru-cache/lru-cache.py
@@ -0,0 +1,74 @@
+class LRUCache:
+
+    ## Solution 1: Double Linked List
+
+    def __init__(self, capacity: int):
+        self.n = capacity
+        self.d = {}
+        self.prev, self.next = {}, {}
+        self.head, self.tail = "H", "T"
+        self.connect(self.head, self.tail)
+    
+    def connect(self, A, B):
+        self.prev[B] = A
+        self.next[A] = B
+    
+    def delete(self, key):
+        self.connect(self.prev[key], self.next[key])
+        del self.d[key], self.prev[key], self.next[key]
+    
+    def add(self, key, val):
+        self.d[key] = val
+        self.connect(self.prev["T"], key)
+        self.connect(key, "T")
+        if len(self.d) > self.n:
+            self.delete(self.next["H"])
+        
+    def get(self, key: int) -> int:
+        if key not in self.d:
+            return -1
+        val = self.d[key]
+        self.delete(key)
+        self.add(key, val)
+        return val
+    
+    def put(self, key: int, value: int) -> None:
+        if key in self.d:
+            self.delete(key)
+        
+        self.add(key, value)
+        
+
+    """
+    ## Solution 2: Ordered Dictionary
+
+    from collections import OrderedDict
+    
+    def __init__(self, capacity: int):
+        self.n = capacity
+        self.d = OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key not in self.d:
+            return -1
+        val = self.d[key]
+        del self.d[key]
+        self.d[key] = val
+        return val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.d:
+            del self.d[key]
+        self.d[key] = value
+        if len(self.d) > self.n:
+            self.d.popitem(last=False)
+    """
+
+    ## S3:
+    ## https://algo.monster/liteproblems/146        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
