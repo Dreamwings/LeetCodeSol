a986ab37bad6ad2007c38f08ef12df116a1cbde5
Dreamwings <huyb2009@gmail.com>
2024-03-27 22:56:42 -0700

    Updated

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,29 +1,20 @@
-class RandomizedSet(object):
-    
+class RandomizedSet:
+
     def __init__(self):
-        
         self.d = collections.defaultdict(int)
         self.v = []
 
-    def insert(self, val):
-        """
-        :type val: int
-        :rtype: bool
-        """
+    def insert(self, val: int) -> bool:
         if val in self.d:
             return False
         self.d[val] = len(self.v)
         self.v.append(val)
         return True
-        
-        
-    def remove(self, val):
-        """
-        :type val: int
-        :rtype: bool
-        """
+
+    def remove(self, val: int) -> bool:
         if val not in self.d:
             return False
+        
         i = self.d[val]
         t = self.v[-1]
         self.v[i] = t
@@ -31,15 +22,11 @@ class RandomizedSet(object):
         self.v.pop()
         del self.d[val]
         return True
-        
 
-    def getRandom(self):
-        """
-        :rtype: int
-        """
+    def getRandom(self) -> int:
         
         return random.choice(self.v)
-        
+
 
 # Your RandomizedSet object will be instantiated and called as such:
 # obj = RandomizedSet()
