7bc030f188ee567e9c708e620cdf1ccd73e682ba
Dreamwings <huyb2009@gmail.com>
2024-03-29 00:11:56 -0700

    Time: 38 ms (69.74%) | Memory: 16.6 MB (78.86%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -4,22 +4,6 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
 
-        ## S2: Two Pointers from the End
-
-        while m > 0 and n > 0:
-            if nums1[m-1] >= nums2[n-1]:
-                nums1[m+n-1] = nums1[m-1]
-                m -= 1
-            else:
-                nums1[m+n-1] = nums2[n-1]
-                n -= 1
-            # note while condition must be m > 0 and n > 0 because 
-            # we use index m - 1 and n - 1, so m - 1 >= 0, n - 1 >= 0 
-            
-        if n > 0: nums1[:n] = nums2[:n]
-            
-        """
-
         ## S1: Three Pointers from the End
 
         if n == 0: return nums1
@@ -39,4 +23,21 @@ class Solution:
         if j >= 0: nums1[:k+1] = nums2[:j+1]
         
         """
+
+        ## S2: Two Pointers from the End
+
+        while m > 0 and n > 0:
+            if nums1[m-1] >= nums2[n-1]:
+                nums1[m+n-1] = nums1[m-1]
+                m -= 1
+            else:
+                nums1[m+n-1] = nums2[n-1]
+                n -= 1
+            # note while condition must be m > 0 and n > 0 because 
+            # we use index m - 1 and n - 1, so m - 1 >= 0, n - 1 >= 0 
+            
+        if n > 0: nums1[:n] = nums2[:n]
+            
+        """
+
                 
