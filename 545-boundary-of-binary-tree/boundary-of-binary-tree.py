65a8bec1648c1cdcf01c8201b1926d1b37d58248
Dreamwings <huyb2009@gmail.com>
2024-03-30 15:43:03 -0700

    545 updated

--- 545-boundary-of-binary-tree/boundary-of-binary-tree.py
+++ 545-boundary-of-binary-tree/boundary-of-binary-tree.py
@@ -7,6 +7,38 @@
 class Solution:
     def boundaryOfBinaryTree(self, root: Optional[TreeNode]) -> List[int]:
         
+        ## S3: DFS
+        ## T: O(N)
+        ## S: O(N)
+
+        # The main idea is to carry the flag isleft and isight
+        # in the dfs steps to help decide when to add node
+        # values to the boundary array.
+        if not root: return []
+        boundary = [root.val]
+        def dfs(root, isleft, isright):
+            if root:
+                # append when going down from the left or at leaf node
+                if (not root.left and not root.right) or isleft:
+                    boundary.append(root.val)
+                
+                if root.left and root.right:
+                    dfs(root.left, isleft, False)
+                    dfs(root.right, False, isright)
+                else:
+                    dfs(root.left,  isleft, isright)
+                    dfs(root.right, isleft, isright)
+                
+                # append to boundary when coming up from the right
+                if (root.left or root.right) and isright:
+                    boundary.append(root.val)
+        
+        dfs(root.left, True, False)
+        dfs(root.right, False, True)
+        return boundary
+
+
+        """
         ## S1: DFS
         ## T: O(N)
         ## S: O(N)
@@ -49,3 +81,4 @@ class Solution:
 
         ## S2: DFS
         ## AlgoMonster
+        """
