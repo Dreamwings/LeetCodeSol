8809a4a1fb8185d51dcf7eebbb3bec60a6aac26c
Dreamwings <huyb2009@gmail.com>
2024-03-29 21:57:02 -0700

    Time: 85 ms (9.94%) | Memory: 20.6 MB (8.64%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,6 +1,6 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        """
+        
         ## S1: Sorting
         ## T: O(NlogN)
         ## S: O(N)
@@ -18,7 +18,7 @@ class Solution:
         
         return res
         
-        
+        """
         ## S2:
         ## T: O(1000010) -> O(1)
         ## S: O(1000010) -> O(1)
@@ -36,21 +36,23 @@ class Solution:
         # "max" to find the maximum number of concurrent meetings
         return max(accumulate(meeting_delta))
 
-        """
+        
         ## S3: Heap 
-        ## T: O(NlogN)
+        ## T: O(NlogN) + O(NlogK) ~ O(NlogN)
         ## S: O(N)
 
         n = len(intervals)
         if n<=1: return n
         heap = []
-        
-        for start, end in sorted(intervals):
+
+        for start, end in sorted(intervals): # O(NlogN)
             if heap and start >= heap[0]:
-                heapq.heappushpop(heap, end)
+                heapq.heappushpop(heap, end) # O(logK)
             else:
                 heapq.heappush(heap, end)
+        # Time complexity for the for loop is O(NlogK)
+        
         return len(heap)
 
-        
+        """
         
