ac9fb382d55b166730973bdd1791ceebfa36531b
Dreamwings <huyb2009@gmail.com>
2024-03-29 18:11:22 -0700

    2060 updated

--- 2060-check-if-an-original-string-exists-given-two-encoded-strings/check-if-an-original-string-exists-given-two-encoded-strings.py
+++ 2060-check-if-an-original-string-exists-given-two-encoded-strings/check-if-an-original-string-exists-given-two-encoded-strings.py
@@ -1,6 +1,44 @@
 class Solution:
     def possiblyEquals(self, s1: str, s2: str) -> bool:
 
+        ## S2: DFS + Cache
+
+        from functools import lru_cache
+    
+        @lru_cache(None)
+        def dfs(s1, s2, i, j, diff):
+            if i == len(s1) and j == len(s2):
+                return diff == 0
+            if i < len(s1) and s1[i].isdigit():
+                k = i
+                val = 0
+                while k < len(s1) and s1[k].isdigit():
+                    val = val*10 + int(s1[k])
+                    k += 1
+                    if dfs(s1, s2, k, j, diff - val):
+                        return True
+            elif j < len(s2) and s2[j].isdigit():
+                k = j
+                val = 0
+                while k < len(s2) and s2[k].isdigit():
+                    val = val*10 + int(s2[k])
+                    k += 1
+                    if dfs(s1, s2, i, k, diff + val):
+                        return True
+            elif diff == 0:
+                if i < len(s1) and j < len(s2) and s1[i] == s2[j] and dfs(s1, s2, i+1, j+1, diff):
+                    return True
+            elif diff > 0:
+                if i < len(s1) and dfs(s1, s2, i+1, j, diff - 1):
+                    return True
+            elif diff < 0:
+                if j < len(s2) and dfs(s1, s2, i, j+1, diff + 1):
+                    return True
+            return False
+        
+        return dfs(s1, s2, 0, 0, 0)
+        
+
         """
         ## S3: DP
         ## https://algo.monster/liteproblems/2060
@@ -42,41 +80,3 @@ class Solution:
 
         
         """
-
-        ## S2: DFS + Cache
-
-        from functools import lru_cache
-    
-        @lru_cache(None)
-        def dfs(s1, s2, i, j, diff):
-            if i == len(s1) and j == len(s2):
-                return diff == 0
-            if i < len(s1) and s1[i].isdigit():
-                k = i
-                val = 0
-                while k < len(s1) and s1[k].isdigit():
-                    val = val*10 + int(s1[k])
-                    k += 1
-                    if dfs(s1, s2, k, j, diff - val):
-                        return True
-            elif j < len(s2) and s2[j].isdigit():
-                k = j
-                val = 0
-                while k < len(s2) and s2[k].isdigit():
-                    val = val*10 + int(s2[k])
-                    k += 1
-                    if dfs(s1, s2, i, k, diff + val):
-                        return True
-            elif diff == 0:
-                if i < len(s1) and j < len(s2) and s1[i] == s2[j] and dfs(s1, s2, i+1, j+1, diff):
-                    return True
-            elif diff > 0:
-                if i < len(s1) and dfs(s1, s2, i+1, j, diff - 1):
-                    return True
-            elif diff < 0:
-                if j < len(s2) and dfs(s1, s2, i, j+1, diff + 1):
-                    return True
-            return False
-        
-        return dfs(s1, s2, 0, 0, 0)
-        
