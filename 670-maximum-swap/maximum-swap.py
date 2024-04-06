e3db39be3a8de6d82d4fe05c7ec69531a27eb855
Dreamwings <huyb2009@gmail.com>
2024-04-06 16:23:46 -0700

    Time: 39 ms (24.17%) | Memory: 16.5 MB (86.98%) - LeetSync

--- 670-maximum-swap/maximum-swap.py
+++ 670-maximum-swap/maximum-swap.py
@@ -3,9 +3,11 @@ class Solution:
         
         from collections import defaultdict
         
-        a = list(map(int, str(num)))  # note in Python 3, need to use list for map function
+        ## S1:
+
+        a = list(map(int, str(num)))  # Note in Python 3, need to use list for map function
         # a = [int(x) for x in str(num)]
-        last = defaultdict(int)
+        last = defaultdict(int) # Must have a default value for each from "0123456789"
         for i, x in enumerate(a):
             last[x] = i
         
@@ -16,3 +18,37 @@ class Solution:
                     return int(''.join([str(x) for x in a]))
         
         return num
+
+        """
+
+        ## S2:
+
+        digits = list(str(num))
+        n = len(digits)
+        # Create a list to keep track of the indices of the digits
+        # that should be considered for swapping.
+        last = list(range(n))
+      
+        # Populate the last list with the index of the maximum digit
+        for i in range(n - 2, -1, -1):
+            # If the current digit is less than or equal to the maximum digit found
+            # to its right, update the last for the current position.
+            if digits[i] <= digits[last[i + 1]]:
+                last[i] = last[i + 1]
+      
+        # Loop through each digit to find the first instance where a swap would
+        # increase the value of the number. Swap and break the loop.
+        for i in range(n):
+            max_index = last[i]
+            # If the current digit is smaller than the maximum digit to its right,
+            # swap the two digits.
+            if digits[i] < digits[max_index]:
+                # Swap the current digit with the maximum digit found.
+                digits[i], digits[max_index] = digits[max_index], digits[i]
+                # Only one swap is needed, so break after swapping.
+                break
+      
+        # Convert the list of digits back to a string and then to an integer.
+        return int(''.join(digits))
+
+        """
