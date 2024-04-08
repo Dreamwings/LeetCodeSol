099caa0221d338add8987288df609dddde2efcd5
Dreamwings <huyb2009@gmail.com>
2024-04-07 18:11:45 -0700

    Time: 36 ms (78.21%) | Memory: 16.7 MB (21.09%) - LeetSync

--- 249-group-shifted-strings/group-shifted-strings.py
+++ 249-group-shifted-strings/group-shifted-strings.py
@@ -4,15 +4,18 @@ class Solution:
         ## S1:
         ## T: O(NK), K is avg string len
         ## S: O(N)
+
         d = defaultdict(list)
+
         for s in strings:
             key = []
             for i in range(len(s) - 1):
-                # Throw in 26 so that we can normalzie below
-                diff = 26 + ord(s[i+1]) - ord(s[i])
-                # Wrap around
-                # z + 1 = a
-                key.append(str(diff % 26))
+                # Calculate the relative ord diff between each latter char and the first char
+                # Add 26 to make sure the diff is always positive, normalize it with mod 26
+                # Note diff is an int in [0, 1, 2, ..., 25]
+                diff = (26 + ord(s[i+1]) - ord(s[0])) % 26
+                # Wrap around: z + 1 = a
+                key.append(str(diff))
             d[','.join(key)].append(s)
-
+        
         return list(d.values())        
