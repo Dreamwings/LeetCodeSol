01fae0f714d6a3d171bfb9495ab582726f9d82ea
Dreamwings <huyb2009@gmail.com>
2024-04-07 19:15:56 -0700

    Time: 33 ms (72.51%) | Memory: 16.7 MB (24.21%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,6 +1,28 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        
+
+        ## S2
+
+        def quick_power(base, exp):
+            res = 1.0
+            # Continue multiplying the base until the exponent is zero
+            while exp:
+                # If exponent is odd, multiply the res by the base
+                if exp & 1:
+                    res *= base
+                # Square the base (equivalent to base = pow(base, 2))
+                base *= base
+                # Right shift exponent by 1 (equivalent to dividing by 2)
+                exp >>= 1
+            return res
+
+        if n >= 0: 
+            return quick_power(x, n) 
+        else:
+            return 1 / quick_power(x, -n)
+
+        """
+                
         ## S1:
         if n == 0: return 1
         if x == 0:
@@ -19,23 +41,6 @@ class Solution:
             x = x * x
             
         return p
+
         """   
-        ## S2
-
-        def quick_power(base: float, exponent: int) -> float:
-            result = 1.0
-            # Continue multiplying the base until the exponent is zero
-            while exponent:
-                # If exponent is odd, multiply the result by the base
-                if exponent & 1:
-                    result *= base
-                # Square the base (equivalent to base = pow(base, 2))
-                base *= base
-                # Right shift exponent by 1 (equivalent to dividing by 2)
-                exponent >>= 1
-            return result
-
-        # If n is non-negative, call quick_power with x and n directly.
-        # Otherwise, calculate the reciprocal of the positive power.
-        return quick_power(x, n) if n >= 0 else 1 / quick_power(x, -n)
-        """        
+        
