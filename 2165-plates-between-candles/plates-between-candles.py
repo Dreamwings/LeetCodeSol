f29bd03d773f7c3ae610b6a685d35300642edc6b
Dreamwings <huyb2009@gmail.com>
2024-04-11 20:02:59 -0700

    Time: 1454 ms (89.50%) | Memory: 56.5 MB (19.46%) - LeetSync

--- 2165-plates-between-candles/plates-between-candles.py
+++ 2165-plates-between-candles/plates-between-candles.py
@@ -2,7 +2,8 @@ class Solution:
     def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
         
         ## S2: 
-
+        ## T: O(N + Q)
+        ## S: O(N + Q)
 
         n = len(s)
       
