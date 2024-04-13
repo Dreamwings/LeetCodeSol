c4a1fb7347d56f49020ad825d5beaa1c187839a9
Dreamwings <huyb2009@gmail.com>
2024-04-13 00:28:50 -0700

    Time: 303 ms (8.82%) | Memory: 16.5 MB (72.65%) - LeetSync

--- 830-largest-triangle-area/largest-triangle-area.py
+++ 830-largest-triangle-area/largest-triangle-area.py
@@ -21,3 +21,11 @@ class Solution:
                     max_area = max(max_area, area)
         
         return max_area
+
+        """
+        ## S2:
+
+        return max(0.5 * abs(i[0] * j[1] + j[0] * k[1] + k[0] * i[1]- j[0] * i[1] - k[0] * j[1] - i[0] * k[1])
+            for i, j, k in itertools.combinations(p, 3))
+
+        """
