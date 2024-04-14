3238b19a37b2ffc2f70687f13e65f577612b8df6
Dreamwings <huyb2009@gmail.com>
2024-04-14 16:24:10 -0700

    Time: 38 ms (52.59%) | Memory: 16.7 MB (37.66%) - LeetSync

--- 71-simplify-path/simplify-path.py
+++ 71-simplify-path/simplify-path.py
@@ -2,6 +2,8 @@ class Solution:
     def simplifyPath(self, path: str) -> str:
 
         ## S1: Stack
+        ## T: O(N)
+        ## S: O(N)
 
         stack = []
 
@@ -13,8 +15,11 @@ class Solution:
         
         return "/" + "/".join(stack)
 
-        """
+
+
         ## S2: Stack
+        ## T: O(N)
+        ## S: O(N)
         
         stack = []
         arr = [x for x in path.split('/') if x != '.' and x != '']
@@ -27,5 +32,5 @@ class Solution:
                 stack.append(c)
         
         return '/' + '/'.join(stack)
-        """
+
         
