fe52cf538b655e1b7ee36a015670d06bb5344cc1
Dreamwings <huyb2009@gmail.com>
2024-04-14 16:54:45 -0700

    921

--- 921-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
+++ 921-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
@@ -4,6 +4,10 @@ class Solution(object):
         :type S: str
         :rtype: int
         """
+
+        ## S1: 
+        ## T: O(N)
+        ## S: O(1)
         
         if S == '': 
             return 0
@@ -18,3 +22,17 @@ class Solution(object):
                 missing_l += 1
         
         return missing_l + missing_r
+
+
+        ## S2: 
+        ## T: O(N)
+        ## S: O(1)
+
+        ans = bal = 0
+        for symbol in S:
+            bal += 1 if symbol == '(' else -1
+            # It is guaranteed bal >= -1
+            if bal == -1:
+                ans += 1
+                bal += 1
+        return ans + bal
