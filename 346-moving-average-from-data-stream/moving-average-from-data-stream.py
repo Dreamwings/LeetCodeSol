51cebeaa8cdcc813b7baf377fa53a120b983f656
Dreamwings <huyb2009@gmail.com>
2024-04-14 16:20:29 -0700

    346: Add a solution

--- 346-moving-average-from-data-stream/moving-average-from-data-stream.py
+++ 346-moving-average-from-data-stream/moving-average-from-data-stream.py
@@ -1,3 +1,7 @@
+## S1: Array + Pointers
+## T: O(1) for next
+## S: O(N)
+
 class MovingAverage:
 
     def __init__(self, size: int):
@@ -21,6 +25,30 @@ class MovingAverage:
         # need to consider self.i < self.n:
         return 1.0 * self.presum / min(self.i, self.n)
                 
+
+## S2: Double-ended Queue
+## T: O(1) for next
+## S: O(N)
+
+from collections import deque
+
+class MovingAverage:
+    def __init__(self, size: int):
+        self.size = size
+        self.queue = deque()
+        # number of elements seen so far
+        self.window_sum = 0
+        self.count = 0
+
+    def next(self, val: int) -> float:
+        self.count += 1
+        # calculate the new sum by shifting the window
+        self.queue.append(val)
+        tail = self.queue.popleft() if self.count > self.size else 0
+
+        self.window_sum = self.window_sum - tail + val
+
+        return self.window_sum / min(self.size, self.count)
 
 
 # Your MovingAverage object will be instantiated and called as such:
