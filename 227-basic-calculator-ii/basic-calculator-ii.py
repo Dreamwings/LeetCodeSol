9ae2371937731abb5b56a6896e0d31c69450fccb
Dreamwings <huyb2009@gmail.com>
2024-04-14 13:22:06 -0700

    Time: 377 ms (5.04%) | Memory: 19.5 MB (68.26%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,6 +1,39 @@
 class Solution:
     def calculate(self, s: str) -> int:
+
+        ## S2: No Stack
+        ## T: O(N)
+        ## S: O(1)
+
+        s += "+"
+        pre_op, pre_v, v, res = "+", 0, 0, 0
+
+        for i, c in enumerate(s):
+            print(pre_v, v, res)
+            if c.isdigit():
+                v = 10 * v + int(c)
+            elif c in "+-*/":
+                if pre_op == "+":
+                    res += pre_v
+                    pre_v = v
+                elif pre_op == "-":
+                    res += pre_v
+                    pre_v = -v
+                elif pre_op == "*":
+                    pre_v = pre_v * v
+                elif pre_op == "/":
+                    # if pre_v < 0:
+                    #     pre_v = math.ceil(pre_v / v)
+                    # else:
+                    #     pre_v = pre_v // v
+                    pre_v = int(pre_v / v)
+                
+                v, pre_op = 0, c
         
+        return res + pre_v
+
+
+
         ## S1: Stack
         ## T: O(N)
         ## S: O(N)
