ff28482d3070b1cabecbb8030fd17f357cda04bd
Dreamwings <huyb2009@gmail.com>
2024-04-14 11:29:41 -0700

    314: add different solutions

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
@@ -7,7 +7,33 @@
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         
-        ## S1: BFS
+        from collections import defaultdict, deque
+
+        ## S2: BFS, No Sorting
+        ## T: O(N)
+        ## S: O(N)
+
+        if not root: return []
+        d = defaultdict(list)
+        q = deque([(root, 0)])
+        min_col = max_col = 0
+
+        while q:
+            node, col = q.popleft()
+            min_col = min(min_col, col)
+            max_col = max(max_col, col)
+            d[col].append(node.val)
+
+            if node.left:
+                q.append((node.left, col - 1))
+            if node.right:
+                q.append((node.right, col + 1))
+        
+        return [d[k] for k in range(min_col, max_col + 1)]
+
+        
+
+        ## S1: BFS + Sorting
         ## T: O(NlogN)
         ## S: O(N)
         
@@ -23,8 +49,37 @@ class Solution:
             if node.right:
                 q.append((node.right, x + 1))
         
-        res = []
-        for k in sorted(d):
-            res.append(d[k])
+        return [d[k] for k in sorted(d)]
+
         
-        return res
+        ## S3: DFS
+        ## T: O(W * HlogH), W is the width, H is the height
+        ## S: O(N)
+
+        if root is None:
+            return []
+
+        columnTable = defaultdict(list)
+        min_column = max_column = 0
+
+        def DFS(node, row, column):
+            if node is not None:
+                nonlocal min_column, max_column
+                columnTable[column].append((row, node.val))
+                min_column = min(min_column, column)
+                max_column = max(max_column, column)
+
+                # preorder DFS
+                DFS(node.left, row + 1, column - 1)
+                DFS(node.right, row + 1, column + 1)
+
+        DFS(root, 0, 0)
+
+        # order by column and sort by row
+        ret = []
+        for col in range(min_column, max_column + 1):
+            columnTable[col].sort(key=lambda x:x[0])
+            colVals = [val for row, val in columnTable[col]]
+            ret.append(colVals)
+
+        return ret
