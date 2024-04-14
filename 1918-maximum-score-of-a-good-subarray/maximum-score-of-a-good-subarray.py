58c881804a65544e3aec40262e2a0b48afe24438
Dreamwings <huyb2009@gmail.com>
2024-04-13 23:43:23 -0700

    Time: 902 ms (56.03%) | Memory: 27 MB (47.56%) - LeetSync

--- 1918-maximum-score-of-a-good-subarray/maximum-score-of-a-good-subarray.py
+++ 1918-maximum-score-of-a-good-subarray/maximum-score-of-a-good-subarray.py
@@ -1,6 +1,28 @@
 class Solution:
     def maximumScore(self, nums: List[int], k: int) -> int:
         
+        ## S3: Greedy, Two Pointers
+        ## T: O(N)
+        ## S: O(1)
+
+        n = len(nums)
+        left = right = k 
+        ans = nums[k]
+        curr_min = nums[k]
+        
+        while left > 0 or right < n - 1:
+            if (nums[left - 1] if left else 0) < (nums[right + 1] if right < n - 1 else 0):
+                right += 1
+                curr_min = min(curr_min, nums[right])
+            else:
+                left -= 1
+                curr_min = min(curr_min, nums[left])
+
+            ans = max(ans, curr_min * (right - left + 1))
+        
+        return ans
+
+
         ## S2: Monotonic Stack
         ## T: O(N)
         ## S: O(N)
@@ -30,7 +52,7 @@ class Solution:
         
         return ans
 
-        
+
 
         ## S1: Binary Search
         ## T: O(NlogN)
