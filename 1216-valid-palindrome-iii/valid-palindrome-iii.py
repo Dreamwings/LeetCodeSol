094639fa416ef8a9e8ae51eadbbc50b1f1db3bc3
Dreamwings <huyb2009@gmail.com>
2024-04-15 23:27:20 -0700

    1178 to 1216

--- 1216-valid-palindrome-iii/valid-palindrome-iii.py
+++ 1216-valid-palindrome-iii/valid-palindrome-iii.py
@@ -1,6 +1,28 @@
 class Solution:
     def isValidPalindrome(self, s: str, k: int) -> bool:
 
+        ## S2: DFS + Memory
+        ## T: O(N^2) because at each char, the branching factor is 2, and no duplicated computing due to cache
+        ## S: O(N^2)
+
+        # Simple lightweight unbounded function cache, the same as lru_cache(maxsize=None)
+        @cache 
+        def isValid(i, j, kk):
+            # check if s[i:j+1] is valid by removing at most kk chars
+            if kk < 0:
+                return False
+            if i >= j:
+                return True
+                
+            if s[i] == s[j]:
+                return isValid(i + 1, j - 1, kk)
+            else:
+                return isValid(i + 1, j, kk - 1) or isValid(i, j - 1, kk - 1)
+
+        return isValid(0, len(s) - 1, k)
+
+
+
         ## S3: DP
         ## T: O(N^2)
         ## S: O(N^2)
@@ -28,7 +50,7 @@ class Solution:
       
         return False
 
-        """
+        
         
         ## S1: Two Pointers + BFS
         ## T: O(N^2)
@@ -65,19 +87,3 @@ class Solution:
                 visited[l][r-1] = True
 
 
-        ## S2: DFS + Memory
-
-        @cache
-        def isValid(i, j, available):
-            if available < 0:
-                return False
-            elif i >= j:
-                return True
-            elif s[i] == s[j]:
-                return isValid(i + 1, j - 1, available)
-            else:
-                return isValid(i + 1, j, available - 1) or isValid(i, j - 1, available - 1)
-
-        return isValid(0, len(s) - 1, k)
-
-        """
