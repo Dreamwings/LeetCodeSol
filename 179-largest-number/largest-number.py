849e9bcabedf6f17c6f953c2a8a56cfc5982e915
Dreamwings <huyb2009@gmail.com>
2024-04-21 16:40:09 -0700

    Time: 40 ms (65.54%) | Memory: 16.5 MB (27.96%) - LeetSync

--- 179-largest-number/largest-number.py
+++ 179-largest-number/largest-number.py
@@ -1,10 +1,37 @@
+## S2:
+## T: O(NlogK), N = len(nums), K is avg length of all str(num)
+## S: O(N * K)
+
+class Solution:
+    def largestNumber(self, nums):
+
+        if len(nums) == 1:
+            return str(nums[0])
+
+        nums_str = list(map(str, nums))
+        max_len = max(map(len, nums_str))
+
+        nums_str.sort(key=lambda x: x * (max_len // len(x) + 1), reverse=True)
+        # print(nums_str)
+        
+        return ''.join(nums_str) if int(nums_str[0]) != 0 else '0'
+
+
+
+"""
+## S1:
+## T: O(NlogK), N = len(nums), K is avg length of all str(num)
+## S: O(N * K)
+
 class LargerNumKey(str):
     def __lt__(x, y):
-        return x+y > y+x
+        return x + y > y + x
         
 class Solution:
     def largestNumber(self, nums):
 
         largest_num = ''.join(sorted(map(str, nums), key=LargerNumKey))
-        
+
         return '0' if largest_num[0] == '0' else largest_num
+
+"""
