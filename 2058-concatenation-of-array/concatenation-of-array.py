04c00145e3ae7ddbd1162f3d115f91150b1ca6d9
Dreamwings <huyb2009@gmail.com>
2024-04-23 22:00:13 -0700

    Time: 76 ms (19.44%) | Memory: 16.9 MB (20.42%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.py
+++ 2058-concatenation-of-array/concatenation-of-array.py
@@ -1,4 +1,12 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
         
+        ## S1:
+
         return nums * 2
+
+        
+        ## S2:
+        
+        nums.extend(nums)
+        return nums
