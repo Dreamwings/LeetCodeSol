6f310cd136acfc531f4ff38d9566d07fe7f6d9e7
Dreamwings <huyb2009@gmail.com>
2024-05-27 18:55:49 -0700

    346 updated

--- 346-moving-average-from-data-stream/moving-average-from-data-stream.py
+++ 346-moving-average-from-data-stream/moving-average-from-data-stream.py
@@ -17,9 +17,9 @@ class MovingAverage:
         # cal average with self.n numbers
         # when self.i < self.n, v, cal average with self.i numbers
 
-        x = self.i % self.n
-        self.presum += val - self.a[x]
-        self.a[x] = val
+        k = self.i % self.n
+        self.presum += val - self.a[k]
+        self.a[k] = val
         self.i += 1
 
         # need to consider self.i < self.n:
@@ -34,21 +34,21 @@ from collections import deque
 
 class MovingAverage:
     def __init__(self, size: int):
-        self.size = size
-        self.queue = deque()
-        # number of elements seen so far
+        self.n = size
+        self.q = deque()
         self.window_sum = 0
-        self.count = 0
+        self.count = 0 # number of elements seen so far
 
     def next(self, val: int) -> float:
         self.count += 1
         # calculate the new sum by shifting the window
-        self.queue.append(val)
-        tail = self.queue.popleft() if self.count > self.size else 0
+        self.q.append(val)
+        tail = self.q.popleft() if self.count > self.n else 0
 
         self.window_sum = self.window_sum - tail + val
 
-        return self.window_sum / min(self.size, self.count)
+        return self.window_sum / min(self.n, self.count)
+   
 
 
 # Your MovingAverage object will be instantiated and called as such:
