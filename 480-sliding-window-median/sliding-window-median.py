b3d0e7692d1f5c123f8ff2ae3446a22c8f3f040f
Dreamwings <huyb2009@gmail.com>
2024-05-29 01:02:46 -0700

    480 updated

--- 480-sliding-window-median/sliding-window-median.py
+++ 480-sliding-window-median/sliding-window-median.py
@@ -29,23 +29,7 @@ class Solution:
         return medians
 
         
-
-        ## S1: Sort + Insort
-        ## Time: O(N*K*logK) 
-        ## https://leetcode.com/problems/sliding-window-median/discuss/96337/Python-SortedArray-(beats-100)-and-2-Heap(beats-90)-solution
         
-        win, rv = nums[:k], []
-        win.sort()
-        odd = k%2
-        for i,n in enumerate(nums[k:],k):
-            rv.append((win[k//2]+win[k//2-1])/2. if not odd else win[(k-1)//2]*1.)
-            win.pop(bisect(win, nums[i-k])-1) # <<< bisect is faster than .remove()
-            insort(win, nums[i])
-        rv.append((win[k//2]+win[k//2-1])/2. if not odd else win[(k-1)//2]*1.)
-        return rv
-        
-        
-
         ## S2: Min Heap and Max Heap
         ## Time: O(N logK)
         
@@ -77,5 +61,21 @@ class Solution:
             else:
                 ans.append((hi[0] - lo[0]) / 2.0)
         return ans
+        
+        
 
+        
+        ## S3: Sort + Insort
+        ## Time: O(N*K*logK) 
+        ## https://leetcode.com/problems/sliding-window-median/discuss/96337/Python-SortedArray-(beats-100)-and-2-Heap(beats-90)-solution
+        
+        win, rv = nums[:k], []
+        win.sort()
+        odd = k%2
+        for i,n in enumerate(nums[k:],k):
+            rv.append((win[k//2]+win[k//2-1])/2. if not odd else win[(k-1)//2]*1.)
+            win.pop(bisect(win, nums[i-k])-1) # <<< bisect is faster than .remove()
+            insort(win, nums[i])
+        rv.append((win[k//2]+win[k//2-1])/2. if not odd else win[(k-1)//2]*1.)
+        return rv
         
