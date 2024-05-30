e7bb1d7946de0747065e4fc0292780dcb648aed1
Dreamwings <huyb2009@gmail.com>
2024-05-29 19:32:28 -0700

    Time: 45 ms (67.05%) | Memory: 16.7 MB (35.25%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -22,20 +22,19 @@ class Solution:
         
         return lo
 
-        """
-        while lo <= hi :
-            m = (lo + hi) // 2
-            if m == 0 and nums[m] > nums[m+1]:
-                return m
-            elif m == n-1 and nums[m] > nums[m-1]:
-                return m
-            elif nums[m-1] < nums[m] > nums[m+1]:
-                return m
-            elif nums[m] < nums[m+1]:
-                lo = m + 1
-            else:
-                hi = m - 1
+
         
-        return -1
-        """
+        ## S2: Binary Search (Recursive)
+        ## T: O(logN)
+        ## S: O(logN)
+
+        def search(l, r):
+            if l == r:
+                return l
+            m = (l + r) // 2
+            if nums[m] > nums[m + 1]:
+                return search(l, m)
+            return search(m + 1, r)
         
+        return search(0, len(nums) - 1)
+
